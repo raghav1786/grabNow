fd80260d2bb70236902607a04edfddbee00f725a
Raghav <222raghav@gmail.com>
2020-10-31 11:29:44 +0530

    MVVM structure for project
    - added topOfferVC and VM
    - added offerDetailsVC and VM
    - groups for VC VM and Models

---
+++ grabNow/grabNow/ViewController/TopOffersViewController.swift
@@ -0,0 +1,11 @@
+
+import UIKit
+
+class TopOffersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
