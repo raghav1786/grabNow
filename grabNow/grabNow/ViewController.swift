f027e5ee6a87a8a8bea3f6f53645464ee6293ede
Raghav <222raghav@gmail.com>
2020-10-31 10:26:19 +0530

    grabNow - basic project structure
    -created basic project structure
    - added ios app

---
+++ grabNow/grabNow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  grabNow
+//
+//  Created by RAGHAV SHARMA on 31/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
